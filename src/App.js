ce5d2824743a88cc8fd2c41876c3ac80adac0a3c
HoaNguyen <hoanguyenquoc2002@gmail.com>
2023-02-03 16:09:51 +0700

    Pagination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.scss';
 import ColorBox from './components/ColorBox';
 import { useState, useEffect } from 'react';
+import queryString from 'query-string';
+
 import TodoList from './components/TodoList ';
 import TodoForm from './components/TodoForm';
 import PostList from './components/PostList';
+import Pagination from'./components/Pagination';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -13,18 +16,29 @@ function App() {
     ]);
 
     const [postList, setPostList] = useState([])
+    const [pagination, setPagination] = useState({
+      _page: 1, 
+      _limit: 10,
+      _totalRows: 1,
+    });
+    const [filters, setFilters] = useState({
+      _limit: 10,
+      _page: 1,
+    });
 
     useEffect(() => {
       async function fetchPostList() {
 
         try {
-          const requese = 'http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1';
+          const paramsString = queryString.stringify(filters)
+          const requese = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
           const response = await fetch(requese)
           const responseJSON = await response.json();
           console.log({ responseJSON })
 
-          const { data } = responseJSON;
+          const { data, pagination } = responseJSON;
           setPostList(data)
+          setPagination(pagination)
         } catch (error) {
           alert('Failed to fetch post list')
         }
@@ -33,7 +47,15 @@ function App() {
       }
 
       fetchPostList()
-    }, [])
+    }, [filters])
+
+    function handlePageChange(newPage) {
+      console.log('New page', newPage);
+      setFilters({
+        ...filters,
+        _page: newPage,
+      })
+    }
 
     function handleTodoClick(todo) {
       console.log(todo)
@@ -66,6 +88,10 @@ function App() {
       {/* <TodoList todos={todoList} onTodoClick={handleTodoClick}/> */}
       {/* <ColorBox /> */}
       <PostList posts={postList}/>
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
